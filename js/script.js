32b87941f967e2b746f2f2dd6a593a68dfb2d337
Francesco Casadei <fran.casadei@gmail.com>
2019-01-24 09:18:58 +0100

    style ok, javascript ok

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-
-
 let userChoice;
 let generatorChoice;
+// sentences for generatorOne
 let first = 
 [
     [   'Peace,','Love','Money','iPhones','Beauty','Happines'],
@@ -16,12 +15,14 @@ let second = [
   [ ' good',' awesome',' great',' red',' black'],
 ];
 
-// trying generators
+// random generator
 function generator(arr) {
     quote = "";
     quote = arr[0][Math.floor(Math.random()*arr[0].length)];
     quote += arr[1][Math.floor(Math.random()*arr[1].length)];
     quote += arr[2][Math.floor(Math.random()*arr[2].length)];
+
+    return quote
 };
 function generators() {
     var mix = document.getElementById("userInput").value;
@@ -36,46 +37,11 @@ function generators() {
             sentence += generator(second) + '. <br> ';
         }
     }
-    //console.log(sentence);
     document.getElementById('demo').innerHTML = sentence.toString();
 }
 
-//generators
-/*
-function generatorOne() {
-  var picks = [];
-  for (let i = 0; i < first.length; i++) {
-    picks.push(first[i][Math.floor(Math.random() * first[i].length)]);
-  }
-  return picks.join(" ");
-}
+let button = document.getElementById("make-quote");
 
-
-function generatorTwo() {
-  var nicks = [];
-  for (let i = 0; i < second.length; i++) {
-    nicks.push(second[i][Math.floor(Math.random() * second[i].length)]);
-  }
-  return nicks.join(" ");
-}
-
-function generators (){
-    var mix = document.getElementById("userInput").value;
-    //console.log(mix);
-    let sentence = '';
-    const element = document.getElementById("quoteChoice");
-    //console.log(element.value);
-    //console.log(document.getElementById("quoteChoice"));
-        if (element.value === '1'){
-            for (var i = 0; i < mix; i++) {
-              sentence += generatorOne() + '. <br>';
-            }
-        } else if (element.value === '2') {
-            for (var i = 0; i< mix; i++) {
-            sentence += generatorTwo() + '. <br> ';
-        }
-    }
-    //console.log(sentence);
-    document.getElementById('demo').innerHTML = sentence.toString();
-}
-*/
+button.addEventListener("click",function(){
+  generators()
+})
